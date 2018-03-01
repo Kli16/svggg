4490ac1d5166441ba6915b5a72fe81576dc754cf
Kevin Li <kevinli@dwightshrute.home>
2018-03-01 01:31:16 -0500

    blooop

--- svg.js
+++ svg.js
@@ -1,18 +1,17 @@
-
-var toggle = document.getElementById("toggle_button");
-var clear = document.getElementById("clear_button");
-var toggle_status = true;
+var svg = document.getElementById("svg");
+var clear_button = document.getElementById("clear_button");
 var prevX;
 var prevY;
 var started = false;
 
-var shape_maker = function(e) {
+var draw = function(e) {
     var c1 = document.createElementNS("http://www.w3.org/2000/svg", "circle");
     c1.setAttribute("cx", e.offsetX);
     c1.setAttribute("cy", e.offsetY);
     c1.setAttribute("r", "20");
     c1.setAttribute("fill", "yellow");
     c1.setAttribute("stroke", "yellow");
+    svg.appendChild(c1);
     if (!started){
       started = true;
     }
@@ -22,19 +21,17 @@ var shape_maker = function(e) {
       line.setAttribute("y1", prevY);
       line.setAttribute("x2", e.offsetX);
       line.setAttribute("y2", e.offsetY);
+      line.setAttribute("stroke", "#000000");
+      svg.appendChild(line);
     }
     prevX = e.offsetX;
     prevY = e.offsetY;
 }
 
-var clear_canvas = function(e) {
-  context.clearRect(0, 0, canvas.width, canvas.height);
+var clear = function(e) {
+  svg.innerHTML = "";
   started = false;
 }
 
-var toggler = function(e) {
-  toggle_status = !toggle_status;
-}
-
-clear.addEventListener("click", clear_canvas);
-toggle.addEventListener("click", toggler);
+clear_button.addEventListener("click", clear);
+svg.addEventListener("click", draw);
